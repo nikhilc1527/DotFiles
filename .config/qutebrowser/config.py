f562a298b9704fd450a034c1ad01b0900a12295f
Nikhil Chatterjee <nikhilc1527@gmail.com>
2022-11-10 17:57:02 -0600

    Thu Nov 10 05:57:02 PM CST 2022

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -35,6 +35,8 @@ config.set("content.javascript.can_access_clipboard", True)
 
 config.set("content.headers.user_agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36")
 
+config.set("content.user_stylesheets", "/home/nikhilc/.config/qutebrowser/stylesheets/gruvbox/darculized-all-sites.css")
+
 # Change the argument to True to still load settings configured via autoconfig.yml
 config.load_autoconfig(False)
 
